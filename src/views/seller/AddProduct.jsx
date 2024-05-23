4d23dbaee1c7a5e46391984b62f46fed087cec26
Yana Syahrudin <yanna6710@gmail.com>
2024-05-23 14:38:28 +0700

    add product page design part 4

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -49,11 +49,18 @@ const AddProduct = () => {
 
   const [cateShow, setCateShow] = useState(false);
   const [category, setCategory] = useState("");
-  const [allCategory, setAllCategory] = useState([]);
+  const [allCategory, setAllCategory] = useState(categorys);
   const [searchValue, setSearchValue] = useState("");
 
   const categorySearch = (e)=>{
     const value = e.target.value
+    setSearchValue(value)
+    if (value) {
+      let srcValue = allCategory.filter(c => c.name.toLowerCase().indexOf(value.toLowerCase())>-1)
+      setAllCategory(srcValue)
+    } else {
+      setAllCategory(categorys)
+    }
   }
 
   return (
@@ -116,6 +123,7 @@ const AddProduct = () => {
                 >
                   <div className="w-full px-4 py-2 fixed">
                     <input
+                    value={searchValue}
                     onChange={categorySearch}
                       className="px-3 py-1 w-full focus:border-indigo-500 outline-none bg-transparent border border-slate-700 rounded-md text-[#d0d2d6]  overflow-hidden"
                       
@@ -125,9 +133,9 @@ const AddProduct = () => {
                   </div>
                 
                 <div className="pt-14"></div>
-                <div className="flex justify-start items-start flex-col h-[200px] overflow-x-scroll">
+                <div className="flex justify-start items-start flex-col h-[200px] overflow-x-scrool">
                   {
-                  allCategory.map((c, i) => <span onClick={() => {
+                  allCategory.map((c, i) => <span className={`px-4 py-2 hover:bg-indigo-500 hover:text-white hover:shadow-lg w-full cursor-pointer ${category === c.name && 'bg-indigo-500'}`} onClick={() => {
                         setCateShow(false);
                         setCategory(c.name);
                         setSearchValue("");
