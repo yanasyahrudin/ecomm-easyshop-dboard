bc0a8f52f17ba207eb58c17783c9191cfd00491f
Yana Syahrudin <yanna6710@gmail.com>
2024-05-24 09:50:50 +0700

    product image add manage part 1

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { IoMdImages } from "react-icons/io";
 
 const AddProduct = () => {
   const categorys = [
@@ -64,6 +65,19 @@ const AddProduct = () => {
       setAllCategory(categorys);
     }
   };
+
+  const [images, setImages ] = useState([])
+  const [imageShow, setImageShow] = useState([])
+
+  const imageHandle = (e) => {
+    const files = e.target.files
+    const length = files.length
+    if (length > 0) {
+      setImages([...images, ...files])
+    }
+  }
+
+  console.log(images);
 
   return (
     <div className="px-2 lg:px-7 pt-5">
@@ -197,7 +211,7 @@ const AddProduct = () => {
               </div>
             </div>
 
-            <div className="flex flex-col w-full gap-1">
+            <div className="flex flex-col w-full gap-1 mb-5">
               <label htmlFor="description" className="text-[#d0d2d6]">Description</label>
               <textarea
                 className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
@@ -210,6 +224,17 @@ const AddProduct = () => {
                 rows="4"
               ></textarea>
             </div>
+
+            <div className="grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-3 w-full text-[#d0d2d6] mb-4">
+              <label className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-[#d0d2d6]" htmlFor="image">
+                <span><IoMdImages /></span>
+                <span>Select Image</span>
+              </label>
+
+              <input className="hidden" onChange={imageHandle} multiple type="file" id="image" />
+            </div>
+
+
           </form>
         </div>
       </div>
