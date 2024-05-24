d78173323c2ad342ed35490ac72147e213235e39
Yana Syahrudin <yanna6710@gmail.com>
2024-05-24 17:10:30 +0700

    product image add manage part 2

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -66,18 +66,36 @@ const AddProduct = () => {
     }
   };
 
-  const [images, setImages ] = useState([])
-  const [imageShow, setImageShow] = useState([])
+  const [images, setImages] = useState([]);
+  const [imageShow, setImageShow] = useState([]);
 
   const imageHandle = (e) => {
-    const files = e.target.files
-    const length = files.length
+    const files = e.target.files;
+    const length = files.length;
     if (length > 0) {
-      setImages([...images, ...files])
+      setImages([...images, ...files]);
+      let imageUrl = [];
+      for (let i = 0; i < length; i++) {
+        imageUrl.push({ url: URL.createObjectURL(files[i]) });
+      }
+      setImageShow([...imageShow, ...imageUrl]);
     }
-  }
+  };
 
   console.log(images);
+  console.log(imageShow);
+
+  const changeImage = (img, index) => {
+    if (img) {
+      let tempUrl = imageShow;
+      let tempImages = images;
+
+      tempImages[index] = img;
+      tempUrl[index] = { url: URL.createObjectURL(img) };
+      setImageShow([...tempUrl]);
+      setImages([...tempImages])
+    }
+  };
 
   return (
     <div className="px-2 lg:px-7 pt-5">
@@ -212,7 +230,9 @@ const AddProduct = () => {
             </div>
 
             <div className="flex flex-col w-full gap-1 mb-5">
-              <label htmlFor="description" className="text-[#d0d2d6]">Description</label>
+              <label htmlFor="description" className="text-[#d0d2d6]">
+                Description
+              </label>
               <textarea
                 className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
                 onChange={inputHandle}
@@ -226,15 +246,42 @@ const AddProduct = () => {
             </div>
 
             <div className="grid lg:grid-cols-4 grid-cols-1 md:grid-cols-3 sm:grid-cols-2 sm:gap-4 md:gap-4 gap-3 w-full text-[#d0d2d6] mb-4">
-              <label className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-[#d0d2d6]" htmlFor="image">
-                <span><IoMdImages /></span>
+              {imageShow.map((img, i) => (
+                <div className="h-[180px] relative">
+                  <label htmlFor={i}>
+                    <img
+                      className="w-full h-full rounded-sm"
+                      src={img.url}
+                      alt=""
+                    />
+                  </label>
+                  <input
+                    onChange={(e) => changeImage(e.target.files[0], i)}
+                    type="file"
+                    id={i}
+                    className="hidden"
+                  />
+                </div>
+              ))}
+
+              <label
+                className="flex justify-center items-center flex-col h-[180px] cursor-pointer border border-dashed hover:border-red-500 w-full text-[#d0d2d6]"
+                htmlFor="image"
+              >
+                <span>
+                  <IoMdImages />
+                </span>
                 <span>Select Image</span>
               </label>
 
-              <input className="hidden" onChange={imageHandle} multiple type="file" id="image" />
+              <input
+                className="hidden"
+                onChange={imageHandle}
+                multiple
+                type="file"
+                id="image"
+              />
             </div>
-
-
           </form>
         </div>
       </div>
