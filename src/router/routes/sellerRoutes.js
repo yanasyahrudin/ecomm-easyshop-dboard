f0199a132696faa254944bd2c852886a57aaf1fc
Yana Syahrudin <yanna6710@gmail.com>
2024-05-29 08:48:03 +0700

    chat seller and admin page design part 1

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -6,6 +6,8 @@ const Products = lazy(()=> import('../../views/seller/Products'))
 const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts'))
 const Orders = lazy(()=> import('../../views/seller/Orders'))
 const Payments = lazy(()=> import('../../views/seller/Payments'))
+const SellerToAdmin = lazy(()=> import('../../views/seller/SellerToAdmin'))
+const SellerToCostumer = lazy(()=> import('../../views/seller/SellerToCustomer'))
 
 export const sellerRoutes = [
     {
@@ -46,6 +48,23 @@ export const sellerRoutes = [
     {
         path: '/seller/dashboard/payments',
         element: <Payments/>,
+        role: 'seller',
+        status: 'active'
+    },
+    {
+        path: '/seller/dashboard/chat-support',
+        element: <SellerToAdmin/>,
+        ability: ['active', 'deactive', 'pending']
+    },
+    {
+        path: '/seller/dashboard/chat-customer/:customerId',
+        element: <SellerToCostumer/>,
+        role: 'seller',
+        status: 'active'
+    },
+    {
+        path: '/seller/dashboard/chat-customer',
+        element: <SellerToCostumer/>,
         role: 'seller',
         status: 'active'
     },
