d8643a0edfa6ab0053cb274ad4bfc4266c0e37b5
Yana Syahrudin <yanna6710@gmail.com>
2024-05-30 09:08:07 +0700

    seller profile page design part 2

--- src/views/seller/Profile.jsx
+++ src/views/seller/Profile.jsx
@@ -5,7 +5,7 @@ import { FaImages } from "react-icons/fa";
 import { FadeLoader } from "react-spinners";
 
 const Profile = () => {
-  const image = false;
+  const image = true;
   const loader = true;
 
   return (
@@ -15,7 +15,19 @@ const Profile = () => {
           <div className="w-full p-4 bg-[#6a5fdf] rounded-md text-[#d0d2d6]">
             <div className="flex justify-center items-center py-3">
               {image ? (
-                <label htmlFor=""></label>
+                <label
+                  htmlFor="img"
+                  className="h-[150px] w-[200px] relative p-3 cursor-pointer overflow-hidden"
+                >
+                  <img src="http://localhost:3000/images/demo.jpg" alt="" />
+                  {!loader && (
+                    <div className="bg-slate-600 absolute left-0 top-0 w-full h-full opacity-70 flex justify-center items-center z-20">
+                      <span>
+                        <FadeLoader />
+                      </span>
+                    </div>
+                  )}
+                </label>
               ) : (
                 <label
                   className="flex justify-center items-center flex-col h-[150px] w-[200px] cursor-pointer border border-dashed hover:border-red-500 border-[#d0d2d6] relative"
@@ -34,6 +46,7 @@ const Profile = () => {
                   )}
                 </label>
               )}
+              <input type="file" className="hidden"  id="img"/>
             </div>
           </div>
         </div>
