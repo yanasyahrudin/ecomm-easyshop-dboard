99bccae9d2f1bf6fdbe05496e392023c5937e833
Yana Syahrudin <yanna6710@gmail.com>
2024-06-04 08:51:19 +0700

    seller register and login part 5

--- src/store/Reducers/authReducer.js
+++ src/store/Reducers/authReducer.js
@@ -1,68 +1,83 @@
-import { createAsyncThunk, createSlice} from '@reduxjs/toolkit'
-import api from '../../api/api';
+/** @format */
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/api";
 
 export const admin_login = createAsyncThunk(
-    'auth/admin_login',
-    async(info,{rejectWithValue, fulfillWithValue}) => {
-        console.log(info);
-        try {
-            const {data} = await api.post('/admin-login', info, {withCredentials: true})
-            localStorage.setItem('accessToken', data.token)
-            // console.log(data);
-            return fulfillWithValue(data)
-        } catch (error) {
-            // console.log(error.response.data);
-            return rejectWithValue(error.response.data)
-        }
+  "auth/admin_login",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    console.log(info);
+    try {
+      const { data } = await api.post("/admin-login", info, {
+        withCredentials: true,
+      });
+      localStorage.setItem("accessToken", data.token);
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data);
+      return rejectWithValue(error.response.data);
     }
-)
+  }
+);
 
 export const seller_register = createAsyncThunk(
-    'auth/seller_register',
-    async(info,{rejectWithValue, fulfillWithValue}) => {
-        
-        try {
-            console.log(info);
-            const {data} = await api.post('/seller-register', info, {withCredentials: true})
-            // localStorage.setItem('accessToken', data.token)
-            console.log(data);
-            return fulfillWithValue(data)
-        } catch (error) {
-            // console.log(error.response.data);
-            return rejectWithValue(error.response.data)
-        }
+  "auth/seller_register",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      console.log(info);
+      const { data } = await api.post("/seller-register", info, {
+        withCredentials: true,
+      });
+      localStorage.setItem("accessToken", data.token);
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data);
+      return rejectWithValue(error.response.data);
     }
-)
+  }
+);
 
 export const authReducer = createSlice({
-    name: 'auth',
-    initialState: {
-        successMessage: '',
-        errorMessage: '',
-        loader: false,
-        userInfo: ''
+  name: "auth",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    userInfo: "",
+  },
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = "";
     },
-    reducers: {
-        messageClear: (state, _)=>{
-            state.errorMessage = ""
-        }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(admin_login.pending, (state, { payload }) => {
+        state.loader = true;
+      })
+      .addCase(admin_login.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error;
+      })
+      .addCase(admin_login.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message;
+      })
 
-    },
-    extraReducers:(builder)=>{
-        builder
-        .addCase(admin_login.pending, (state, { payload })=> {
-            state.loader = true;
-        })
-        .addCase(admin_login.rejected, (state, { payload })=> {
-            state.loader = false;
-            state.errorMessage = payload.error
-        })
-        .addCase(admin_login.fulfilled, (state, { payload })=> {
-            state.loader = false;
-            state.successMessage = payload.message
-        })
-    }
-})
+      .addCase(seller_register.pending, (state, { payload }) => {
+        state.loader = true;
+      })
+      .addCase(seller_register.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error;
+      })
+      .addCase(seller_register.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message;
+      });
+  },
+});
 
-export const {messageClear} = authReducer.actions
-export default authReducer.reducer
+export const { messageClear } = authReducer.actions;
+export default authReducer.reducer;
--- src/views/auth/Register.jsx
+++ src/views/auth/Register.jsx
@@ -1,21 +1,22 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { useSelector } from "react-redux";
 import { PropagateLoader } from "react-spinners";
 import { overrideStyle } from "../../utils/utils";
-import { useDispatch } from 'react-redux';
-import { seller_register } from "../../store/Reducers/authReducer";
-
+import { useDispatch } from "react-redux";
+import { seller_register, messageClear } from "../../store/Reducers/authReducer";
+import toast from "react-hot-toast";
 
 const Register = () => {
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch()
-
-  const { loader } = useSelector((state) => state.auth);
+  const { loader, successMessage, errorMessage } = useSelector(
+    (state) => state.auth
+  );
 
   const [state, setState] = useState({
     name: "",
@@ -32,10 +33,20 @@ const Register = () => {
 
   const submit = (e) => {
     e.preventDefault();
-    dispatch(seller_register(state))
+    dispatch(seller_register(state));
   };
 
- 
+  useEffect(() => {
+    if (successMessage) {
+      toast.success(successMessage);
+      dispatch(messageClear());
+    }
+    if (errorMessage) {
+      toast.error(errorMessage);
+      dispatch(messageClear());
+    }
+  }, [successMessage, errorMessage]);
+
   return (
     <div className="min-w-screen min-h-screen bg-[#cdcae9] flex justify-center items-center">
       <div className="w-[350px] text-[#ffffff] p-2">
@@ -100,10 +111,15 @@ const Register = () => {
               </label>
             </div>
 
-             <button disabled={loader? true: false} className="bg-slate-800 w-full hover:shadow-blue-300/hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
-              {
-                loader ? <PropagateLoader color='#fff'cssOverride={overrideStyle}/> : 'Sign Up'
-              }
+            <button
+              disabled={loader ? true : false}
+              className="bg-slate-800 w-full hover:shadow-blue-300/hover:shadow-lg text-white rounded-md px-7 py-2 mb-3"
+            >
+              {loader ? (
+                <PropagateLoader color="#fff" cssOverride={overrideStyle} />
+              ) : (
+                "Sign Up"
+              )}
             </button>
 
             <div className="flex item-center mb-3 gap-3 justify-center">
