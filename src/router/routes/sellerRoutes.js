248a7b38a8797de3892c351133e582975634a3c0
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-07-21 19:54:51 +0700

    user role manage part 3 (fixing bug)

--- src/router/routes/sellerRoutes.js
+++ src/router/routes/sellerRoutes.js
@@ -1,95 +1,89 @@
-/** @format */
+import { lazy } from "react";             
 
-import { lazy } from "react";
-const SellerDashboard = lazy(() =>
-  import("../../views/seller/SellerDashboard")
-);
-const AddProduct = lazy(() => import("../../views/seller/AddProduct"));
-const Products = lazy(() => import("../../views/seller/Products"));
-const DiscountProducts = lazy(() =>
-  import("../../views/seller/DiscountProducts")
-);
-const Orders = lazy(() => import("../../views/seller/Orders"));
-const Payments = lazy(() => import("../../views/seller/Payments"));
-const SellerToAdmin = lazy(() => import("../../views/seller/SellerToAdmin"));
-const SellerToCostumer = lazy(() =>
-  import("../../views/seller/SellerToCustomer")
-);
-const Profile = lazy(() => import("../../views/seller/Profile"));
-const EditProduct = lazy(() => import("../../views/seller/EditProduct"));
-const OrderDetails = lazy(() => import("../../views/seller/OrderDetails"));
+const SellerDashboard = lazy(()=> import('../../views/seller/SellerDashboard'))   
+const AddProduct = lazy(()=> import('../../views/seller/AddProduct'))   
+const Products = lazy(()=> import('../../views/seller/Products')) 
+const DiscountProducts = lazy(()=> import('../../views/seller/DiscountProducts')) 
+const Orders = lazy(()=> import('../../views/seller/Orders')) 
+const Payments = lazy(()=> import('../../views/seller/Payments'))
+const SellerToAdmin = lazy(()=> import('../../views/seller/SellerToAdmin'))
+const SellerToCustomer = lazy(()=> import('../../views/seller/SellerToCustomer'))
+const Profile = lazy(()=> import('../../views/seller/Profile'))
+const EditProduct = lazy(()=> import('../../views/seller/EditProduct'))
+const OrderDetails = lazy(()=> import('../../views/seller/OrderDetails'))
 
 export const sellerRoutes = [
-  {
-    path: "/seller/dashboard",
-    element: <SellerDashboard />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/add-product",
-    element: <AddProduct />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/products",
-    element: <Products />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/discount-product",
-    element: <DiscountProducts />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/orders",
-    element: <Orders />,
-    role: "seller",
-    visibility: ["active", "deactive"],
-  },
-  {
-    path: "/seller/dashboard/payments",
-    element: <Payments />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/chat-support",
-    element: <SellerToAdmin />,
-    role: "seller",
-    visibility: ["active", "deactive", "pending"],
-  },
-  {
-    path: "/seller/dashboard/chat-customer/:customerId",
-    element: <SellerToCostumer />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/chat-customer",
-    element: <SellerToCostumer />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/profile",
-    element: <Profile />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/edit-product/:productId",
-    element: <EditProduct />,
-    role: "seller",
-    status: "active",
-  },
-  {
-    path: "/seller/dashboard/order/details/:orderId",
-    element: <OrderDetails />,
-    role: "seller",
-    visibility: ["active", "deactive"],
-  },
-];
+    
+    {
+        path: '/seller/dashboard',
+        element : <SellerDashboard/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/add-product',
+        element : <AddProduct/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/edit-product/:productId',
+        element : <EditProduct/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/products',
+        element : <Products/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/discount-product',
+        element : <DiscountProducts/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/orders',
+        element : <Orders/>,
+        role : 'seller',
+        ability : ['active','deactive']
+    },
+    {
+        path: '/seller/dashboard/order/details/:orderId',
+        element : <OrderDetails/>,
+        role : 'seller',
+        ability : ['active','deactive']
+    },
+    {
+        path: '/seller/dashboard/payments',
+        element : <Payments/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/chat-support',
+        element : <SellerToAdmin/>,
+        ability : ['active','deactive','pending']
+    },
+    {
+        path: '/seller/dashboard/chat-customer/:customerId',
+        element : <SellerToCustomer/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/chat-customer',
+        element : <SellerToCustomer/>,
+        role : 'seller',
+        status : 'active'
+    },
+    {
+        path: '/seller/dashboard/profile',
+        element : <Profile/>,
+        role : 'seller',
+        status : 'active'
+    }
+
+]
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -3,11 +3,10 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 const Home = () => {
-    const { role } = useSelector((state) => state.auth);
-    if (role==='seller')return <Navigate to='/seller/dashboard' replace/>
-    else if (role==='admin')return <Navigate to='/admin/dashboard' replace/>
-    else return <Navigate to='/login' replace/>
-
+   const { role } = useSelector(state => state.auth)
+   if (role === 'seller') return <Navigate to='/seller/dashboard' replace /> 
+   else if (role === 'admin') return <Navigate to='/admin/dashboard' replace />   
+   else return <Navigate to='/login' replace /> 
 };
 
 export default Home;
