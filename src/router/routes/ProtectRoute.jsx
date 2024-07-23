f2752ab31b677d8768315405444870338f721a30
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-07-24 05:09:03 +0700

    protect route manage part 1

--- src/router/routes/ProtectRoute.jsx
+++ src/router/routes/ProtectRoute.jsx
@@ -1,43 +1,27 @@
-/** @format */
+import React, { Suspense } from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
-import React, { Suspense } from "react";
-import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+const ProtectRoute = ({route,children}) => {
+    const {role, userInfo} = useSelector(state => state.auth)
 
-const ProtectRoute = ({ route, children }) => {
-  const { role, userInfo } = useSelector((state) => state.auth);
-
-  if (role) {
-    if (userInfo) {
-      if (userInfo.role === route.role) {
-        if (route.status === userInfo.status) {
-          return <Suspense fallback={null}>{children}</Suspense>;
+    if (role) {
+       if (userInfo) {
+        if (userInfo.role === route.role) {
+            return <Suspense fallback={null} >{children}</Suspense>
         } else {
-          if (userInfo.status === "pending") {
-            return <Navigate to="/seller/account-pending" replace />;
-          } else {
-            return <Navigate to="/seller/account-deactive" replace />;
-          }
+            return <Navigate to='/unauthorized' replace />
         }
-
-        // return <Suspense fallback={null}>{children}</Suspense>;
-      } else {
-        if (route.visibility) {
-          if (route.visibility.some(r=>r === userInfo.status)) {
-            return <Suspense fallback={null}>{children}</Suspense>;
-          } else {
-            return <Navigate to="/seller/account-pending" replace />;
-          }
-        } else {
-          return <Suspense fallback={null}>{children}</Suspense>;
-        }
-      }
-    } else {
-      return <Navigate to="/unauthorized" replace />;
+        
+       }
+        
+    }else {
+        return <Navigate to='/login' replace />
     }
-  } else {
-    return <Navigate to="/login" replace />;
-  }
+
+
+
+    
 };
 
 export default ProtectRoute;
