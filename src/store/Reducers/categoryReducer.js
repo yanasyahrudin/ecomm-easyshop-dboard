bb3b0a69e64b092d40f0ebe87e7b671713b55d26
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-09 06:34:37 +0700

    setup category add and get part 7

--- src/store/Reducers/categoryReducer.js
+++ src/store/Reducers/categoryReducer.js
@@ -20,6 +20,26 @@ export const categoryAdd = createAsyncThunk(
     }
   }
 );
+//End Method
+
+export const get_category = createAsyncThunk(
+  "category/get_category",
+  async ({ parPage, page, searchValue }, { rejectWithValue, fulfillWithValue }) => {
+    
+    try {
+        
+      const { data } = await api.get(`/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`, {
+        withCredentials: true,
+      });
+
+      console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const categoryReducer = createSlice({
   name: "category",
--- src/views/admin/Category.jsx
+++ src/views/admin/Category.jsx
@@ -10,9 +10,11 @@ import { overrideStyle } from "../../utils/utils";
 import {
   categoryAdd,
   messageClear,
+  get_category
 } from "../../store/Reducers/categoryReducer";
 import { useDispatch, useSelector } from "react-redux";
 import toast from "react-hot-toast";
+import Search from "../components/Search";
 
 const Category = () => {
   const dispatch = useDispatch();
@@ -64,6 +66,16 @@ const Category = () => {
     }
   }, [successMessage, errorMessage]);
 
+  useEffect(()=>{
+    const obj = {
+      parPage: parseInt(parPage),
+      page: parseInt(currentPage),
+      searchValue
+    }
+    dispatch(get_category(obj))
+  }, [searchValue, currentPage, parPage])
+  
+
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="flex lg:hidden justify-between items-center mb-5 p-4 bg-[#6a5fdf] rounded-md">
@@ -79,21 +91,10 @@ const Category = () => {
       <div className="flex flex-wrap w-full">
         <div className="w-full lg:w-7/12">
           <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
-            <div className="flex justify-between items-center">
-              <select
-                onChange={(e) => setParPage(parseInt(e.target.value))}
-                className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-              >
-                <option value="5">5</option>
-                <option value="10">10</option>
-                <option value="20">20</option>
-              </select>
-              <input
-                className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-                type="text"
-                placeholder="search"
-              />
-            </div>
+            
+            <Search setParPage={setParPage} setSearchValue={setSearchValue} searchValue={searchValue}/>
+
+
 
             <div className="relative overflow-x-auto">
               <table className="w-full text-sm text-left text-[#d0d2d6]">
