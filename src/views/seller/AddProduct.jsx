ba1942bc53a1acb6493e68c43c00c65a86d6ad68
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-17 23:39:49 +0700

    add product setup part 2

--- src/views/seller/AddProduct.jsx
+++ src/views/seller/AddProduct.jsx
@@ -1,38 +1,23 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoMdImages } from "react-icons/io";
-import { IoMdCloseCircle } from 'react-icons/io';
-
+import { IoMdCloseCircle } from "react-icons/io";
+import { useDispatch, useSelector } from "react-redux";
+import { get_category } from "../../store/Reducers/categoryReducer";
 
 const AddProduct = () => {
-  const categorys = [
-    {
-      id: 1,
-      name: "Sports",
-    },
-    {
-      id: 2,
-      name: "Tshirt",
-    },
-    {
-      id: 3,
-      name: "Mobile",
-    },
-    {
-      id: 4,
-      name: "Computer",
-    },
-    {
-      id: 5,
-      name: "Watch",
-    },
-    {
-      id: 6,
-      name: "Pant",
-    },
-  ];
+  const dispatch = useDispatch();
+  const { categorys } = useSelector((state) => state.category);
+
+  useEffect(() => {
+    dispatch(get_category({
+      searchValue: '',
+      parPage:'',
+      page: ''
+    }))
+  }, []);
 
   const [state, setState] = useState({
     name: "",
@@ -52,7 +37,7 @@ const AddProduct = () => {
 
   const [cateShow, setCateShow] = useState(false);
   const [category, setCategory] = useState("");
-  const [allCategory, setAllCategory] = useState(categorys);
+  const [allCategory, setAllCategory] = useState([]);
   const [searchValue, setSearchValue] = useState("");
 
   const categorySearch = (e) => {
@@ -95,29 +80,40 @@ const AddProduct = () => {
       tempImages[index] = img;
       tempUrl[index] = { url: URL.createObjectURL(img) };
       setImageShow([...tempUrl]);
-      setImages([...tempImages])
+      setImages([...tempImages]);
     }
   };
 
-  const removeImage = (i)=>{
-    const filterImage = images.filter((img,index) => index !== i)
-    const filterImageUrl = imageShow.filter((img, index)=> index !== i)
-    setImages(filterImage)
-    setImageShow(filterImageUrl)
-  }
+  const removeImage = (i) => {
+    const filterImage = images.filter((img, index) => index !== i);
+    const filterImageUrl = imageShow.filter((img, index) => index !== i);
+    setImages(filterImage);
+    setImageShow(filterImageUrl);
+  };
+
+  const add = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(()=>{
+    setAllCategory(categorys)
+  }, [categorys])
 
   return (
     <div className="px-2 lg:px-7 pt-5">
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
         <div className="flex justify-between items-center pb-4">
           <h1 className="text-[#d0d2d6] text-xl font-semibold">Add Product</h1>
-          <Link to='/seller/dashboard/products' className="bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2">
+          <Link
+            to="/seller/dashboard/products"
+            className="bg-blue-500 hover:shadow-blue-500/50 hover:shadow-lg text-white rounded-sm px-7 py-2 my-2"
+          >
             All Product
           </Link>
         </div>
 
         <div>
-          <form>
+          <form onSubmit={add}>
             <div className="flex flex-col mb-3 md:flex-row gap-4 w-full text-[#d0d2d6]">
               <div className="flex flex-col w-full gap-1">
                 <label htmlFor="name">Product Name</label>
@@ -270,7 +266,12 @@ const AddProduct = () => {
                     id={i}
                     className="hidden"
                   />
-                  <span onClick={()=>removeImage(i)} className="p-2 z-10 cursor-pointer bg-slate-700 hover:shadow-lg hover:shadow-slate-400/50 text-white absolute top-1 right-1 rounded-full"><IoMdCloseCircle/></span>
+                  <span
+                    onClick={() => removeImage(i)}
+                    className="p-2 z-10 cursor-pointer bg-slate-700 hover:shadow-lg hover:shadow-slate-400/50 text-white absolute top-1 right-1 rounded-full"
+                  >
+                    <IoMdCloseCircle />
+                  </span>
                 </div>
               ))}
 
@@ -294,11 +295,10 @@ const AddProduct = () => {
             </div>
 
             <div className="flex">
-                    <button className="bg-red-500 hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2">
-                      Add Product
-                    </button>
-                  </div>
-
+              <button className="bg-red-500 hover:shadow-red-500/40 hover:shadow-md text-white rounded-md px-7 py-2 my-2">
+                Add Product
+              </button>
+            </div>
           </form>
         </div>
       </div>
