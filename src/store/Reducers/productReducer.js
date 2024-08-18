2037e42054d9a0fc963a66edcc32d49dc60a406e
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-08-19 06:26:35 +0700

    add product setup part 3

--- src/store/Reducers/productReducer.js
+++ src/store/Reducers/productReducer.js
@@ -1,79 +1,79 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import api from "../../api/api"; 
+import api from "../../api/api";
 
 export const add_product = createAsyncThunk(
-    'product/add_product',
-    async({ name,image },{rejectWithValue, fulfillWithValue}) => {
-        try {
-            const formData = new FormData()
-            formData.append('name', name)
-            formData.append('image', image)
-            const {data} = await api.post('/add_product',formData,{withCredentials: true}) 
-            // console.log(data)
-            return fulfillWithValue(data)
-        } catch (error) {
-            // console.log(error.response.data)
-            return rejectWithValue(error.response.data)
-        }
+  "product/add_product",
+  async (product, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post("/product-add", product, {
+        withCredentials: true,
+      });
+      console.log(data)
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
     }
-)
+  }
+);
 
-// End Method 
+// End Method
 
-export const get_product= createAsyncThunk(
-    'category/get_product',
-    async({ parPage,page,searchValue },{rejectWithValue, fulfillWithValue}) => {
-        try {
-            const {data} = await api.get(`/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,{withCredentials: true}) 
-            // console.log(data)
-            return fulfillWithValue(data)
-        } catch (error) {
-            // console.log(error.response.data)
-            return rejectWithValue(error.response.data)
-        }
+export const get_product = createAsyncThunk(
+  "category/get_product",
+  async (
+    { parPage, page, searchValue },
+    { rejectWithValue, fulfillWithValue }
+  ) => {
+    try {
+      const { data } = await api.get(
+        `/category-get?page=${page}&&searchValue=${searchValue}&&parPage=${parPage}`,
+        { withCredentials: true }
+      );
+      // console.log(data)
+      return fulfillWithValue(data);
+    } catch (error) {
+      // console.log(error.response.data)
+      return rejectWithValue(error.response.data);
     }
-)
+  }
+);
 
-  // End Method 
- 
+// End Method
+
 export const productReducer = createSlice({
-    name: 'product',
-    initialState:{
-        successMessage :  '',
-        errorMessage : '',
-        loader: false,
-        products : [] ,
-        totalProduct: 0
+  name: "product",
+  initialState: {
+    successMessage: "",
+    errorMessage: "",
+    loader: false,
+    products: [],
+    totalProduct: 0,
+  },
+  reducers: {
+    messageClear: (state, _) => {
+      state.errorMessage = "";
     },
-    reducers : {
-
-        messageClear : (state,_) => {
-            state.errorMessage = ""
-        }
-
-    },
-    extraReducers: (builder) => {
-        builder
-        .addCase(add_product.pending, (state, { payload }) => {
-            state.loader = true;
-        })
-        .addCase(add_product.rejected, (state, { payload }) => {
-            state.loader = false;
-            state.errorMessage = payload.error
-        }) 
-        .addCase(add_product.fulfilled, (state, { payload }) => {
-            state.loader = false;
-            state.successMessage = payload.message
-            state.products = [...state.products, payload.category]   
-        })
-        .addCase(get_product.fulfilled, (state, { payload }) => {
-            state.totalProduct = payload.totalProduct
-            state.products = payload.products
-        })
- 
-
-    }
-
-})
-export const {messageClear} = productReducer.actions
-export default productReducer.reducer
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(add_product.pending, (state, { payload }) => {
+        state.loader = true;
+      })
+      .addCase(add_product.rejected, (state, { payload }) => {
+        state.loader = false;
+        state.errorMessage = payload.error;
+      })
+      .addCase(add_product.fulfilled, (state, { payload }) => {
+        state.loader = false;
+        state.successMessage = payload.message;
+        state.products = [...state.products, payload.category];
+      })
+      .addCase(get_product.fulfilled, (state, { payload }) => {
+        state.totalProduct = payload.totalProduct;
+        state.products = payload.products;
+      });
+  },
+});
+export const { messageClear } = productReducer.actions;
+export default productReducer.reducer;
