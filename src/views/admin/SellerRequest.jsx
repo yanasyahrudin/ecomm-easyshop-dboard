851dba59b22186e4bc972bd22b88329aa3328a24
yanasyahrudin <11207252@nusamandiri.ac.id>
2024-09-21 06:29:28 +0700

    seller request show add part 1

--- src/views/admin/SellerRequest.jsx
+++ src/views/admin/SellerRequest.jsx
@@ -1,36 +1,41 @@
 /** @format */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Pagination from "./../Pagination";
 import { FaEdit, FaEye, FaImage, FaTrash } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import Search from "../components/Search";
+import {
+  get_seller_request
+} from "../../store/Reducers/sellerReducer";
+
 
 const SellerRequest = () => {
+  const dispatch = useDispatch();
+  const { sellers, totalSeller } = useSelector((state) => state.seller);
+
   const [currentPage, setCurrentPage] = useState(1);
   const [searchValue, setSearchValue] = useState("");
   const [parPage, setParPage] = useState(5);
   const [show, setShow] = useState(false);
 
+  useEffect(()=>{
+    dispatch(get_seller_request({
+      parPage,
+      searchValue,
+      page: currentPage
+    }))
+  }, [parPage, searchValue, currentPage])
+
   return (
+    
     <div className="px-2 lg:px-7 pt-5">
       <h1 className="text-[20px] font-bold mb-3">Seller Request</h1>
-
+      
       <div className="w-full p-4 bg-[#6a5fdf] rounded-md">
-        <div className="flex justify-between items-center">
-          <select
-            onChange={(e) => setParPage(parseInt(e.target.value))}
-            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-          >
-            <option value="5">5</option>
-            <option value="10">10</option>
-            <option value="20">20</option>
-          </select>
-          <input
-            className="px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]"
-            type="text"
-            placeholder="search"
-          />
-        </div>
+      <Search setParPage={setParPage} setSearchValue={setSearchValue} searchValue={searchValue}/>
+
 
         <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left text-[#d0d2d6]">
@@ -39,7 +44,7 @@ const SellerRequest = () => {
                 <th scope="col" className="py-3 px-4">
                   No
                 </th>
-                
+
                 <th scope="col" className="py-3 px-4">
                   Name
                 </th>
@@ -98,7 +103,10 @@ const SellerRequest = () => {
                     className="py-2 px-4 font-medium whitespace-nowrap"
                   >
                     <div className="flex justify-start items-center gap-4">
-                      <Link to='/admin/dashboard/seller/details/2' className="p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50">
+                      <Link
+                        to="/admin/dashboard/seller/details/2"
+                        className="p-[6px] bg-green-500 rounded hover:shadow-lg hover:shadow-green-500/50"
+                      >
                         <FaEye />
                       </Link>
                     </div>
@@ -120,7 +128,8 @@ const SellerRequest = () => {
         </div>
       </div>
     </div>
-  );
+    
+  )
 };
 
 export default SellerRequest;
